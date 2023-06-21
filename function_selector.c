14f29e204bc8b333899f680bf17f43fab3780c0b
ambrose <kolus7381@gmail.com>
2023-06-21 11:58:00 +0100

    msg

---
+++ function_selector.c
@@ -0,0 +1,26 @@
+#include "monty.h"
+/**
+ * select_func - selects a function to execute
+ * @s: is a string passed to the function
+ *
+ * Return: a pointer corresponding to string
+ */
+void (*select_func(char *s))(stack_t **stack, unsigned int line_number)
+{
+	instruction_t op[] = {{"push", push},
+		{"pall", pall},
+		/*{"pint", pint},
+		  {"pop", pop},
+		  {"swap", swap},
+		  {"add", add},*/
+		{"\0", NULL}};
+	int i = 0;
+
+	while (op[i].opcode)
+	{
+		if (!strcmp(s, op[i].opcode))
+			return (op[i].f);
+		i++;
+	}
+	return (NULL);
+}
