0af869b284fa891f3d54fa80f400da154d74e9bb
ambrose <kolus7381@gmail.com>
2023-06-22 07:09:45 +0100

    added_add.c

---
+++ add.c
@@ -0,0 +1,21 @@
+/**
+ * add - Adds the top two elements of the stack.
+ * @stack: Pointer to a pointer pointing to top node of the stack.
+ * @line_number: Interger representing the line number of of the opcode.
+ */
+void add(stack_t **stack, unsigned int line_number)
+{
+	int sum;
+
+	if (stack == NULL || *stack == NULL || (*stack)->next == NULL)
+	{
+		fprintf(stderr, "L%d: can't pop an empty stack\n", line_number);
+		free_linkedlist();
+		exit(EXIT_FAILURE);
+	}
+	(*stack) = (*stack)->next;
+	sum = (*stack)->n + (*stack)->prev->n;
+	(*stack)->n = sum;
+	free((*stack)->prev);
+	(*stack)->prev = NULL;
+}
